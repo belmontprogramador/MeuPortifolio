c2e85f1fe36fa989e9299c6703bd390f16a63e63
Felipe Belmont <belmontprogramador@gmail.com>
2023-05-20 16:00:55 -0300

    animação e acordeon feito

--- Assets/js/acordeon.js
+++ Assets/js/acordeon.js
@@ -3,12 +3,12 @@ const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
 acordeonTriggers.forEach((trigger) => {
     trigger.addEventListener('click', (e) =>{
         const acordeon = trigger.parentElement
-        const isOpen = acordeon.classList.contains("open")
+        const isOpen = acordeon.classList.contains("aberto")
         
         if (isOpen) {
-            acordeon.classList.remove('open')
+            acordeon.classList.remove('aberto')
         }else{
-            acordeon.classList.add('open')
+            acordeon.classList.add('aberto')
         }
         console.log('show')
     })
